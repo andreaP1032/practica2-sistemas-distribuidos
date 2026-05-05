c5033fb6b13619e636aeb862ffd9246abdc07e84
Andrea Perez <apg1032@alu.ubu.es>
2026-05-05 21:11:14 +0200

    añado comentarios en MainController

--- spring-boot-app/src/main/java/com/sistemasdistr/basico/controller/Maincontroller.java
+++ spring-boot-app/src/main/java/com/sistemasdistr/basico/controller/Maincontroller.java
@@ -1,3 +1,11 @@
+/**
+ * Controlador principal de la aplicación
+ * Este controlador expone endpoints REST que permiten interactuar con la aplicación
+ * Se encarga de invocar el servicio FlaskApiService para consumir la API externa simulada (Flask)
+ * y devolver las respuestas al cliente
+ * Forma parte de la capa de presentación dentro de la arquitectura de la aplicación
+ */
+
 package com.sistemasdistr.basico.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -8,12 +16,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Maincontroller {
+	// Endpoint raíz que devuelve la vista principal (index.html)
     @GetMapping("/")
     public String vistaHome( ModelMap interfazConPantalla){
         return "index";
     }
     @Autowired
     private FlaskApiService flaskApiService;
+    
+    // Endpoint que recibe el tipo de prueba y llama a la API Flask mediante el servicio
     @GetMapping("/test/{type}")
     @ResponseBody
     public String testApi(@PathVariable String type) {
